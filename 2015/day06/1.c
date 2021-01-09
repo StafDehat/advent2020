127f3ad04b3e4bc1942cf8eff16f29ecf231a438
Andrew Howard <andrew.howard@rackspace.com>
2021-01-09 01:12:39 -0600

    2015.6.1 done

--- 2015/day06/1.c
+++ 2015/day06/1.c
@@ -3,19 +3,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int lights[1000][1000];
+#define NUMCOLS 1000
+#define NUMROWS 1000
+
+int lights[NUMCOLS][NUMROWS];
+char line[40];
 
 void toggle(int minX, int minY, int maxX, int maxY) {
-  for (int x=minX, x<=maxX, x++) {
-    for (int y=minY, y<=maxY, y++) {
+  for (int x=minX; x<=maxX; x++) {
+    for (int y=minY; y<=maxY; y++) {
       lights[x][y] = ! lights[x][y];
     }
   }
 }
 
 void set(int minX, int minY, int maxX, int maxY, int state) {
-  for (int x=minX, x<=maxX, x++) {
-    for (int y=minY, y<=maxY, y++) {
+  for (int x=minX; x<=maxX; x++) {
+    for (int y=minY; y<=maxY; y++) {
       lights[x][y] = state;
     }
   }
@@ -23,12 +27,35 @@ void set(int minX, int minY, int maxX, int maxY, int state) {
 
 int countLit() {
   int count=0;
-  for (int x=minX, x<=maxX, x++) {
-    for (int y=minY, y<=maxY, y++) {
+  for (int x=0; x<NUMCOLS; x++) {
+    for (int y=0; y<NUMROWS; y++) {
       if ( lights[x][y] )
         count++;
     }
   }
+  return count;
+}
+
+void getCoords(char* line, int* x, int* y) {
+  int tmp = 0;
+  int i = 0;
+  // Find X coord
+  while ( line[i] != ',' ) {
+    if ( line[i] >= '0' && line[i] <= '9' ) {
+      tmp = ( tmp * 10 ) + ( line[i] - '0' );
+    }
+    i++;
+  }
+  *x = tmp;
+  tmp = 0;
+  // Find Y coord
+  while ( line[i] != ' ' && line[i] != '\0' ) {
+    if ( line[i] >= '0' && line[i] <= '9' ) {
+      tmp = ( tmp * 10 ) + ( line[i] - '0' );
+    }
+    i++;
+  }
+  *y = tmp;
 }
 
 int meat(FILE *fp) {
@@ -37,8 +64,23 @@ int meat(FILE *fp) {
     memset(lights[i], 0, 1000*sizeof(int));
   }
 
-  while ( fgets(line,100,fp) ) {
-    
+  int minX;
+  int minY;
+  int maxX;
+  int maxY;
+  // Read the instructions, line-by-line
+  while ( fgets(line,40,fp) ) {
+    // Set min/max X/Y:
+    getCoords(line, &minX, &minY);
+    getCoords(strstr(line, "through"), &maxX, &maxY);
+
+    // Perform the described action
+    if ( strstr(line, "toggle") )
+      toggle(minX, minY, maxX, maxY);
+    else if ( strstr(line, "turn on") )
+      set(minX, minY, maxX, maxY, 1);
+    else if ( strstr(line, "turn off") )
+      set(minX, minY, maxX, maxY, 0);
   }
 
   int numLit = countLit();
